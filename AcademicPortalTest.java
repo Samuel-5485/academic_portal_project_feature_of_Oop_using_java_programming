d48f2b402cafbbc3d5e39b21dbbf921af8ca3217
Samuel-5485 <samediriba54@gmail.com>
2025-12-25 11:33:29 +0300

    I have setup the information related to student and faculty

--- AcademicPortalTest.java
+++ AcademicPortalTest.java
@@ -29,9 +29,11 @@ public class AcademicPortalTest {
         System.out.println("-----------------------------");
 
         //Create Students (inherits from Person)
-        Faculty facult1 = new Faculty("F001", "Dr.Gemachis", "gemachis@ambou.edu.et", "Data Science", "Data Scientist");
-        Faculty faculty2 = new Faculty("F002", "Prof.Ebisa", "ebisa@edu.et", "SoftWare Engineering", "Associate Professor");
-        Faculty faculty3 = new Faculty("F003", "Mr.Debela", "debela1@edu.edu.et", "Electrical Engineering", "Lecturer");
+        Faculty faculty1 = new Faculty("F001", "Dr.Gemachis", "gemachis@ambou.edu.et", "Data Science", "Data Scientist");
+        Faculty faculty2 = new Faculty("F002", "Prof.Ebisa", "ebisa@ambou.edu.et", "SoftWare Engineering", "Associate Professor");
+        Faculty faculty3 = new Faculty("F003", "Mr.Debela", "debela1@ambou.edu.et", "Electrical Engineering", "Lecturer");
+        Faculty faculty4 = new Faculty("F004", "Mr.Samuel", "same@ambou.edu.et", "Computer Science", "Lecturer");
+
 
             System.out.println("\n Created 5 Students and 3 Faculty members.");
 
@@ -52,6 +54,7 @@ public class AcademicPortalTest {
             Portal.addPerson(facult1);
             Portal.addPerson(faculty2);
             Portal.addPerson(faculty3);
+             Portal.addPerson(faculty4);
 
             // Try to add duplicate (should show error)
             System.out.println("\nTrying to add duplicate ID (Testing validation):");
@@ -82,10 +85,110 @@ public class AcademicPortalTest {
          Course oopCourse = new Course("CSO201", "Object-Oriented Programming", 3);
          Course dataBase = new Course("CSD201", "Fundamental Data Base", 3);
          Course linearAlgebra = new Course("CSL201", "Linear Algebra", 3);
-         Course statics = new Course("CST201", "Statics & Probability", 3);
+         Course staticsProbability = new Course("CST201", "Statics & Probability", 3);
+
          
 
          //Add Courses to Portal
-         academicPortal.addCourse();
+         academicPortal.addCourse(oopCourse);
+         academicPortal.addCourse(dataBase);
+         academicPortal.addCourse(linearAlgebra);
+         academicPortal.addCourse(staticsProbability);
+
+         System.out.println("\nDisplaying all Courses:");
+         academicPortal.displayAllCourses();
+
+         //============== DEMONSTATE TWO-WAY RELATIONSHIP ===============
+         System.out.println(" PHASE 5: DEMONSTATING TWO-WAY RELATIONSHIPS");
+         System.out.println("-------------------------------------------");
+
+         // Assign faculty to courses (Updates both Course.faculty And Faculty.teachingCourses )
+         System.out.println("\n1.ASSIGNING FACULTY TO COURSES:");
+         System.out.println("--------------------------------------");
+         academicPortal.assignFaculyToCourse("F001", "CSO201"); //Dr.Gemachis
+         academicPortal.assignFaculyToCourse("F002", "CSD201");
+         academicPortal.assignFaculyToCourse("F003", "CSL201");
+         academicPortal.assignFaculyToCourse("F004", "CST201");
+         
+
+        System.out.println("\n2. ENROLLING STUDENTS IN COURSES:");
+        System.out.println("------------------------------------------");
+        System.out.println("(Each enrollement updates both Course.roster And Student.enrolledCourses)");
+
+        // Enroll Student (Updates both Course.roster and Student.enrolledCourses)
+        academicPortal.enrollStudentInCourse("S001", "CSO201");
+        academicPortal.enrollStudentInCourse("S002", "CSD201");
+        academicPortal.enrollStudentInCourse("S003", "CSL201");
+        academicPortal.enrollStudentInCourse("S004", "CST201");
+        academicPortal.enrollStudentInCourse("S005", "CSD201");
+
+        academicPortal.enrollStudentInCourse("S003", "CS0201");
+
+       
+        // ================ DEMONSTRATE R/Ship INTEGRITY ==================
+        System.out.println("\n PHASE 6: VERIFYING TWO-WAY R/SHIP");
+        System.out.println("-------------------------------------------------");
+
+        // Check that r/ship are mainted on both sides
+        System.out.println("\n. Checking OOP Course details: ");
+        oopCourse.displayCourseInfo();
+
+        System.out.println("\n2. Checking Student 1 enrolled courses:");
+        System.out.println("Abdi IS enrolled in " + student1.getEnrolledInCourses().size() + " courses:");
+
+        for (Course course : student1.getEnrolledInCourses()){
+            System.out.println("  -  " + course.getCourseName());
+
+        }
+
+        System.out.println("\n3. Checking Faculty 1 teaching courses:"); //Dr.Gemachis
+        System.out.println("Dr. Gemachis is teaching " + faculty1.getTeachingCourses().size() + " courses:");
+        for (Course course : faculty1.getTeachingCourses()){
+            System.out.println("  -  " + course.getCourseName());
+        }
+        
+        // ============ DEMONSTRATE SEARCH FUNCTIONALITY ================
+        System.out.println("\n  PHASE 7: DEMONSTRATING SEARCH FUNCTIONALITY");
+        System.out.println("-----------------------------------------");
+
+        // Using static findPerson method
+        System.out.println("\nSearching for person with ID 'S003':");
+        Person foundPerson = Portal.findPerson("S003");
+        if (foundPerson != null){
+            System.out.println("Found: ");
+            foundPerson.displayInfo();
+        }
+
+        System.out.println("\nSeaerching for non-existent ID 'S010':");
+        Person notFound = Portal.findPerson("S010");
+        if (notFound == null){
+            System.out.println("Person not found (returns null)");
+        }
+
+        // ============== DEMONSTRATE GPA CALCULATION ====================
+        System.out.println("\n PHASE 8: DEMONSTRATING GPA CALCULATION");
+        System.out.println("-----------------------------------------");
+
+        //Update some GPAs for demonstration
+        student1.setGpa(3.7);
+        student2.setGpa(3.85);
+        student3.setGpa(3.46);
+        student4.setGpa(3.5);
+
+        System.out.println("\nCalculating Gpas:");
+        academicPortal.calculateStudentGPA("S001");
+        academicPortal.calculateStudentGPA("S002");
+        academicPortal.calculateStudentGPA("S003");
+        academicPortal.calculateStudentGPA("S004");
+
+
+        // ==================== FINAL SYSTEM SUMMARY ==================
+        
+
+
+
+
+         
+
     }
 }
