b67ad2d16623c83bd5026a5a4ecc13f0364267a5
Samuel-5485 <samediriba54@gmail.com>
2025-12-27 22:07:41 +0300

    here this file contains scanner information

--- AcademicPortalMenu.java
+++ AcademicPortalMenu.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class AcademicPortalMenu {
-    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        Portal portal = new Portal();
+
+        System.out.println(" Welcome to Academic Portal");
+        System.out.println("===========================================");
+
+        while (true){
+            System.out.println("\nChoose action:");
+            System.out.println("1. Add Student");
+            System.out.println("2. Add Student");
+            System.out.println("3. Add Student");
+            System.out.println("4. Add Student");
+            System.out.println("5. Add Student");
+            System.out.println("6. Add Student");
+            System.out.println("Choice");
+
+            int choice = scanner.nextInt();
+
+        }
+    }    
 }
